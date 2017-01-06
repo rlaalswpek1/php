231415f54f89899d0615d32a173a551655bd9c17
rlaalswpek <16space6@sdh.hs.kr>
2017-01-06 14:59:02 +0900

    Add files via upload

--- join.php
+++ join.php
@@ -5,12 +5,13 @@ include("header.php");
     <div class="loginbox">
         <div class="boxbox">회원가입</div>
         <form action="" class="form">
-        <label for="">아이디<em>*</em> :<input type="text" size="10" placeholder="아이디" required></label>
-        <label for="">비밀번호<em>*</em> :<input type="password" size="10" placeholder="비밀번호" required></label>
-        <label for="">비밀번호확인<em>*</em> :<input type="password" size="10" placeholder="비밀번호 확인" required></label>
-        <label for="">닉네임<em>*</em> :<input type="text" size="10" placeholder="닉네임" required></label>
-        <label for="">생년월일 :<input type="date" required></label>
-        <label for="">이메일주소 :<input type="email" size="10" placeholder="이메일" required></label>
+            <label for="">아이디<em>*</em> :<input type="text" size="10" placeholder="아이디" required></label>
+            <label for="">비밀번호<em>*</em> :<input type="password" size="10" placeholder="비밀번호" required></label>
+            <label for="">비밀번호확인<em>*</em> :<input type="password" size="10" placeholder="비밀번호 확인" required></label>
+            <label for="">닉네임<em>*</em> :<input type="text" size="10" placeholder="닉네임" required></label>
+            <label for="">생년월일 :<input type="date" required></label>
+            <label for="">이메일주소 :<input type="email" size="10" placeholder="이메일" required></label>
+            <button type="submit">가입하기 </button>
          </form>    
         </div>
 </div> 
--- login.php
+++ login.php
@@ -6,7 +6,7 @@ include("header.php");
         <div class="boxbox">로그인 </div>
         <form action="">
             <input type="text" placeholder="아이디" class="id" name="id" required>
-            <input type="text" placeholder="비밀번호" class="pw" name="pw" required>
+            <input type="password" placeholder="비밀번호" class="pw" name="pw" required>
             <button type="submit">로그인하기 </button>
          </form>    
         </div>

44568b9526d3455aa5073220167bd017621fbdc8
rlaalswpek <16space6@sdh.hs.kr>
2017-03-14 21:02:33 +0900

    Add files via upload

--- view.php
+++ view.php
@@ -1,4 +1,5 @@
 <?php
+    include("dbcon.php");
     include("header.php");
     if(isset($_GET['id'])){
         $id = $_GET['id'];
@@ -6,17 +7,19 @@
     else{
         exit("<script>location.href=''</script>");
     }
+    $sql = "SELECT * FROM notice where id=".$id;
+    $row = $db->query($sql)->fetch();
 ?>
 <div class="mainbox"> 
     <div class="boxbox">글보기</div>
     <form action="write.ok.php" method="post" id='wf'>
          <div class="viewheader">
              <div class="viewtitlelogo">제목</div>
-             <div class="viewtitle"></div>
+             <div class="viewtitle"><?= $row['title'] ?></div>
          </div>
          <div class="viewbody">
-             <div class="viewcontentlogo">내용</div>
-             <div class="viewcontent"></div>
+             <div class="viewcontentlogo">내용</div> 
+             <div class="viewcontent"><?= $row['contents']?></div>
          </div>
     </form>
 </div>
